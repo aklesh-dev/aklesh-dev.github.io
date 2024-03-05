87d1281c95c33d1554b65486cb3bcaae9ed60cb3
aklesh yadav <arzunayavadu998@gmail.com>
2024-03-05 14:45:03 +0545

    update contact section

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,32 @@ const shadowHeader = ()  => {
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm  = document.getElementById('contact-form'),
+    contactMessage  = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_ohsprec','template_i62rebo','#contact-form','hdkz4gvNLIoOJq42K')
+    .then(() => {
+        // Show sent message
+        contactMessage.textContent = "Message sent successfully ✅"
+
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ""
+        }, 5000) //  Time in milliseconds
+
+        // Clear input fields
+        contactForm.reset();
+    }, () => {
+        // Show error message
+        contactMessage.textContent = "Message not sent (service error) ❌"
+    })
+}
+
+contactForm.addEventListener('submit',sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
